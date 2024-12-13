5d44934fcd271ef682ef364558da5fcee2dd9c24
nepidkhadka <dipenkhadka191@gmail.com>
2024-12-13 15:21:28 +0545

    Updated CORS Options

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 };
 app.use(cors(corsOptions));
