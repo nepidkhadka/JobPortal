bdd23a8c378c579e07e519bdd8b6296b8a15dcb1
nepidkhadka <dipenkhadka191@gmail.com>
2024-12-13 15:54:14 +0545

    fix:CORS URL Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin:
+    process.env.FRONTEND_URL || "https://jobportal-frontend-puak.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
