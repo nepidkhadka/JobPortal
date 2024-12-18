eb87904c2b57c14855911685e669a6c19620d944
nepidkhadka <dipenkhadka191@gmail.com>
2024-12-18 16:31:26 +0545

    Populated Company

--- backend/src/controllers/job.controller.js
+++ backend/src/controllers/job.controller.js
@@ -96,9 +96,13 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req, res) => {
   try {
     const jobId = req.params.id;
-    const job = await Job.findById(jobId).populate({
-      path: "applications",
-    });
+    const job = await Job.findById(jobId)
+      .populate({
+        path: "applications",
+      })
+      .populate({
+        path: "company",
+      });
     if (!job) {
       return res.status(404).json({
         message: "Job Not Found",
